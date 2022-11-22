a426a4ac823e24fa31336c70ede4ae5c8493f962
anna223436 <118176457+anna223436@users.noreply.github.com>
2022-11-22 10:23:10 +0000

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -4,5 +4,5 @@ JOB_NAME="run1.sh"
 
 echo "Hello from the Jenkins job named: ${JOB_NAME}"
 
-touch 1.txt 2.txt 3.txt 4.txt 5.txt
-zip archive.zip *.txt
+#touch 1.txt 2.txt 3.txt 4.txt 5.txt
+#zip archive.zip *.txt
